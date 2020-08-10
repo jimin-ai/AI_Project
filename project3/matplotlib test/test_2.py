e41dabaea7a88baca7d7ffe795e1350d2228366d
jimin-ai <ai.titannia@gmail.com>
2020-08-10 10:10:21 +0900

    .

--- project3/matplotlib test/test_2.py
+++ project3/matplotlib test/test_2.py
@@ -23,14 +23,16 @@ class Main(QMainWindow,Ui_MainWindow) :
         labels = ['수도권', '동남권', '충청권', '호남권', '대경권', '강원권', '제주']
         ratio = [50.10, 15.26, 10.67, 9.89, 9.81, 2.97, 1.29]
 
-        pie = cht.Pie()
-        pie.add("대한민국 인구비율", list(zip(labels, ratio)), radius=150)
+        pie = cht.Pie(opts.InitOpts(width='350px', height='350px'))
+        pie.add("대한민국 인구비율", list(zip(labels, ratio)), radius=100)
+        pie.set_global_opts(title_opts=opts.TitleOpts(),
+        toolbox_opts=opts.ToolboxOpts(is_show=False), tooltip_opts = opts .TooltipOpts (is_show = False))
         pie.render()
  
     def mainLayout(self):
         self.we_view.close()
         self.we_view = QWebEngineView(self)
-        self.we_view.load(QUrl("file:///C:/Users/pfjas/GoogleDrive/프로젝트/개발/project/render.html")) 
+        self.we_view.load(QUrl("C:/Users/SMT049/Google Drive/프로젝트/개발/project/render.html")) 
         self.view_layout.addWidget(self.we_view)
         
         # self.setLayout(self.gridLayout)
